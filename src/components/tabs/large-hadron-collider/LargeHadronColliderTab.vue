c6c692fc7771fa2fa4bc1a19e67f419c4fe85e80
Supersonic Seven <b.rockholz@icloud.com>
2026-05-05 09:22:47 -0400

    Add LargeHadronColliderTab component

---
+++ src/components/tabs/large-hadron-collider/LargeHadronColliderTab.vue
@@ -0,0 +1,43 @@
+<script>
+import AcceleratorsPanel from "./AcceleratorsPanel";
+
+export default {
+  name: "PelleTab",
+  components: {
+    AcceleratorsPanel
+  },
+  data() {
+    return {
+      hasAccelerator: false
+    };
+  },
+  methods: {
+    update() {
+      this.hasStrike = Accelerators.all.some(a => a.isUnlocked);
+    },
+  }
+};
+</script>
+
+<template>
+  <div class="l-large-hadron-collider-tab">
+    <div class="l-large-hadron-collider-all-content-container">
+      <AcceleratorsPanel v-if="hasAccelerator" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.l-large-hadron-collider-tab {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.l-large-hadron-collider-all-content-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+}
+</style>
