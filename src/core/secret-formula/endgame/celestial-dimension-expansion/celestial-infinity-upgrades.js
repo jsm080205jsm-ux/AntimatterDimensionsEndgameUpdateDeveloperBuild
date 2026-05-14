ebd539e025df37e2ef2068ac7fd3babf45e5d7e6
Supersonic Seven <b.rockholz@icloud.com>
2026-05-14 12:22:11 -0400

    Change upgrade IDs to string format

--- src/core/secret-formula/endgame/celestial-dimension-expansion/celestial-infinity-upgrades.js
+++ src/core/secret-formula/endgame/celestial-dimension-expansion/celestial-infinity-upgrades.js
@@ -1,19 +1,19 @@
 export const celestialInfinityUpgrades = {
   gameSpeedMultCIP: {
-    id: 1,
+    id: "gameSpeedMultCIP",
     cost: 1,
     description: () => `Gain a multiplier to Game Speed based on unspent Celestial Infinity Points`,
     effect: () => player.disablePostReality ? DC.D1 : Currency.celestialInfinityPoints.value.plus(1).pow(308),
     formatEffect: value => formatX(value, 2, 1)
   },
   celDimPurchaseBoost: {
-    id: 2,
+    id: "celDimPurchaseBoost",
     cost: 10,
     description: () => `Increase the Celestial Dimension Per-Purchase Multiplier to ${formatX(3)}`,
     effect: 3
   },
   alphaDecayStartBoost: {
-    id: 3,
+    id: "alphaDecayStartBoost",
     cost: 20,
     description: () => `Gain a very small boost to Alpha Decay starting time based on Celestial Infinities`,
     effect: () => Decimal.pow(player.endgame.celDimExpansion.celestialInfinities, 0.5).div(300).min(1).add(
@@ -21,37 +21,37 @@ export const celestialInfinityUpgrades = {
     formatEffect: value => `${TimeSpan.fromHours(value).toStringShort()}`
   },
   celDimBoostBuff: {
-    id: 4,
+    id: "celDimBoostBuff",
     cost: 50,
     description: () => `Increase the Celestial Dimension Boost base multiplier to ${formatX(100)}`,
     effect: 100
   },
   celGalaxyBuff: {
-    id: 5,
+    id: "celGalaxyBuff",
     cost: 100,
     description: () => `Increase the Celestial Galaxy buff from ${formatX(1.02, 2, 2)} to ${formatX(1.03, 2, 2)} per Galaxy`,
     effect: 1.03
   },
   celestialMatterConversionBuff: {
-    id: 6,
+    id: "celestialMatterConversionBuff",
     cost: 500,
     description: () => `Increase the Celestial Matter Conversion Rate base from ${formatPow(2)} to ${formatPow(2.5, 1, 1)}`,
     effect: 2.5
   },
   antimatterCelestialDimBuff: {
-    id: 7,
+    id: "antimatterCelestialDimBuff",
     cost: 1000,
     description: "Multiply all Celestial Dimensions by the Antimatter Exponent's Exponent after Alpha Decay",
     effect: () => Decimal.log10(Decimal.log10(player.antimatter.add(1)).add(1)).max(1)
   },
   cipGen: {
-    id: 8,
+    id: "cipGen",
     cost: 2000,
     description: () => `Generate CIP at ${formatPercents(0.1)} of your fastest Celestial Crunch`,
     effect: () => player.records.bestCelestialInfinity.time.times(10)
   },
   buffedStart: {
-    id: 9,
+    id: "buffedStart",
     cost: 4000,
     description: () => `Start Celestial Infinities with ${formatInt(4)} Celestial Dimension Boosts and ${formatInt(2)} Celestial Galaxies`,
     effect: 4
