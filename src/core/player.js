5876dbe22f44549aa8d9c563973b59e343513d1d
Supersonic Seven <b.rockholz@icloud.com>
2026-06-23 10:56:14 -0400

    Add maxNeb property to bestSupernova object

--- src/core/player.js
+++ src/core/player.js
@@ -564,7 +564,8 @@ window.player = {
       time: DC.BEMAX,
       realTime: Number.MAX_VALUE,
       bestNebminTotal: DC.D0,
-      bestSupernovaePerMs: DC.D0
+      bestSupernovaePerMs: DC.D0,
+      maxNeb: DC.D0
     },
     permanent: {
       maxCP: DC.D0,
